2f8c3eb516204f57a402d004cb40c8cef015f208
Zenitsu1112 <163598686+Zenitsu1112@users.noreply.github.com>
2025-06-14 22:07:17 +0530

    Create script.js
    webtunes

---
+++ webtunes/script.js
@@ -0,0 +1,37 @@
+// Redirect to Spotify login
+const CLIENT_ID = 'bbc6b6defc62472a8b4752b6dc82573a';
+const REDIRECT_URI = 'https://austin11.netlify.app/webtunes/'; // or your Netlify site
+
+function redirectToSpotifyLogin() {
+  const scopes = [
+    'user-read-playback-state',
+    'user-read-currently-playing',
+    'user-read-private'
+  ].join('%20');
+
+  window.location.href = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${REDIRECT_URI}&scope=${scopes}`;
+}
+
+document.getElementById('login-btn').addEventListener('click', redirectToSpotifyLogin);
+
+// When redirected back with access token:
+window.onload = () => {
+  const hash = window.location.hash;
+  if (hash) {
+    const token = new URLSearchParams(hash.substring(1)).get('access_token');
+    if (token) {
+      localStorage.setItem('spotify_token', token);
+      loadUserData(token);
+    }
+  }
+};
+
+function loadUserData(token) {
+  fetch('https://api.spotify.com/v1/me', {
+    headers: { Authorization: 'Bearer ' + token }
+  })
+    .then(res => res.json())
+    .then(user => {
+      document.getElementById('user').innerText = `Welcome, ${user.display_name}`;
+    });
+}
